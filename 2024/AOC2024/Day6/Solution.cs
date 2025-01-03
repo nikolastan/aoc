646e516a6b5c0ab00f16528708bb9ea3484dbe87
Nikola Stanojkovic <nikola.stanojkovic@naissus.tech>
2025-01-03 16:43:15 +0100

    Day6 pt2 WIP

--- 2024/AOC2024/Day6/Solution.cs
+++ 2024/AOC2024/Day6/Solution.cs
@@ -1,130 +1,222 @@
 ﻿using NUnit.Framework;
+using System.Diagnostics;
 using Utility.Enums;
+using Utility.Extensions;
 
 namespace Day6;
 public partial class Solution
 {
-	[Test]
-	public void Part1_Example()
-	{
-		var result = SolvePart1("Inputs/example.txt");
-		Assert.That(result, Is.EqualTo(41));
-	}
+    [Test]
+    public void Part1_Example()
+    {
+        var result = SolvePart1("Inputs/example.txt");
+        Assert.That(result, Is.EqualTo(41));
+    }
 
-	[Test]
-	public void Part1_Input()
-	{
-		var result = SolvePart1("Inputs/input.txt");
-		Console.WriteLine(result);
-	}
+    [Test]
+    public void Part1_Input()
+    {
+        var result = SolvePart1("Inputs/input.txt");
+        Console.WriteLine(result);
+    }
 
-	[Test]
-	public void Part2_Example()
-	{
-		var result = SolvePart2("Inputs/example2.txt");
-		Assert.That(result, Is.EqualTo(0));
-	}
+    [Test]
+    public void Part2_Example()
+    {
+        var result = SolvePart2("Inputs/example.txt");
+        Assert.That(result, Is.EqualTo(6));
+    }
 
-	[Test]
-	public void Part2_Input()
-	{
-		var result = SolvePart2("Inputs/input.txt");
-		Console.WriteLine(result);
-	}
+    [Test]
+    public void Part2_Example2()
+    {
+        var result = SolvePart2("Inputs/example2.txt");
+        Assert.That(result, Is.EqualTo(19));
+    }
 
-	int SolvePart1(string inputPath)
-	{
-		var grid = ReadGrid(inputPath, out (int, int) guardPos);
-		var result = 0;
+    [Test]
+    public void Part2_Input()
+    {
+        var result = SolvePart2("Inputs/input.txt");
+        Console.WriteLine(result);
+    }
 
-		var currentDirection = Direction.North;
+    int SolvePart1(string inputPath)
+    {
+        var grid = ReadGrid(inputPath, out (int, int) guardPos);
+        var result = 0;
 
-		while (guardPos.Item1 >= 0 
-			&& guardPos.Item1 < grid.Length 
-			&& guardPos.Item2 >= 0 
-			&& guardPos.Item2 < grid[0].Length)
-		{
-			if (!grid[guardPos.Item1][guardPos.Item2].Traversed)
-			{
-				grid[guardPos.Item1][guardPos.Item2].Traversed = true;
-				result++;
-			}
+        var currentDirection = Direction.North;
 
-			var inFrontOfGuard = CalculateNextMove(guardPos, currentDirection);
+        while (IsValidTile(grid, guardPos))
+        {
+            if (!grid[guardPos.Item1][guardPos.Item2].Traversed)
+            {
+                grid[guardPos.Item1][guardPos.Item2].Traversed = true;
+                result++;
+            }
 
-			if(IsValidMove(grid, inFrontOfGuard))
-			{
-				if (grid[inFrontOfGuard.Item1][inFrontOfGuard.Item2].Char is '#')
-					currentDirection = CalculateNextDirection(currentDirection);
+            var inFrontOfGuard = CalculateNextMove(guardPos, currentDirection);
 
-				guardPos = CalculateNextMove(guardPos, currentDirection);
-			}
-			else
-				guardPos = inFrontOfGuard;
-		}
+            if (IsValidTile(grid, inFrontOfGuard))
+            {
+                if (grid[inFrontOfGuard.Item1][inFrontOfGuard.Item2].Char is '#')
+                    currentDirection = CalculateNextDirection(currentDirection);
 
-		return result; 
-	}
+                guardPos = CalculateNextMove(guardPos, currentDirection);
+            }
+            else
+                guardPos = inFrontOfGuard;
+        }
 
-	int SolvePart2(string inputPath)
-	{
-		//Implement with checking for passed obstacle on right side (with vectors)
-		return 0;
-	}
+        return result;
+    }
 
-	static Tile[][] ReadGrid(string inputPath, out (int, int) start)
-	{
-		(int, int) current = (0, 0);
-		var grid = File.ReadAllLines(inputPath)
-			.Select((x, i) => {
+    int SolvePart2(string inputPath)
+    {
+        var grid = ReadGrid(inputPath, out (int, int) guardPos);
+        var obstacles = new List<(int, int)>();
 
-				if (x.Contains('^'))
-					current = (i, x.IndexOf('^'));
+        var startPos = guardPos;
+        var startDirection = Direction.North;
 
-				return x.Select(c => new Tile(c)).ToArray();
-				})
-			.ToArray();
+        var currentDirection = startDirection;
 
-		start = current;
-		return grid;
-	}
+        while (IsValidTile(grid, guardPos))
+        {
+            var inFrontOfGuard = CalculateNextMove(guardPos, currentDirection);
 
-	static Direction CalculateNextDirection(Direction currentDirection)
-	{
-		return currentDirection switch
-		{
-			Direction.North => Direction.East,
-			Direction.East => Direction.South,
-			Direction.South => Direction.West,
-			Direction.West => Direction.North,
-			_ => throw new InvalidOperationException("Only NESW directions are possible!")
-		};
-	}
+            grid[guardPos.Item1][guardPos.Item2].Traversed = true;
 
-	static (int, int) CalculateNextMove((int, int) currentPos, Direction direction)
-	{
-		return direction switch
-		{
-			Direction.North => (currentPos.Item1 - 1, currentPos.Item2),
-			Direction.South => (currentPos.Item1 + 1, currentPos.Item2),
-			Direction.East => (currentPos.Item1, currentPos.Item2 + 1),
-			Direction.West => (currentPos.Item1, currentPos.Item2 - 1),
-			_ => throw new InvalidOperationException("Only NESW directions are possible!")
-		};
-	}
+            if (IsValidTile(grid, inFrontOfGuard))
+            {
+                var currentTile = grid[inFrontOfGuard.Item1][inFrontOfGuard.Item2];
 
-	static bool IsValidMove(Tile[][] grid, (int, int) move)
-	{
-		return move.Item1 >= 0
-			&& move.Item1 < grid.Length
-			&& move.Item2 >= 0
-			&& move.Item2 < grid[0].Length;
-	}
+                if (inFrontOfGuard != startPos
+                    //&& !currentTile.Traversed
+                    && currentTile.Char is not '#'
+                    && IsLoop(ArrayExtensions.DeepCopy(grid), startPos, inFrontOfGuard, startDirection)
+                    && !obstacles.Contains(inFrontOfGuard))
+                {
+                    obstacles.Add(inFrontOfGuard);
+                }
 
-	struct Tile(char c)
-	{
-		public char Char = c;
-		public bool Traversed = false;
-	}
+                while (grid[inFrontOfGuard.Item1][inFrontOfGuard.Item2].Char is '#')
+                {
+                    currentDirection = CalculateNextDirection(currentDirection);
+
+                    inFrontOfGuard = CalculateNextMove(guardPos, currentDirection);
+                }
+
+                guardPos = CalculateNextMove(guardPos, currentDirection);
+            }
+            else
+                break;
+        }
+
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                if (obstacles.Contains((i, j)))
+                    Debug.Write('O');
+                else
+                    Debug.Write(grid[i][j].Char);
+            }
+
+            Debug.Write('\n');
+        }
+
+
+        return obstacles.Count;
+    }
+
+    static Tile[][] ReadGrid(string inputPath, out (int, int) start)
+    {
+        (int, int) current = (0, 0);
+        var grid = File.ReadAllLines(inputPath)
+            .Select((x, i) =>
+            {
+
+                if (x.Contains('^'))
+                    current = (i, x.IndexOf('^'));
+
+                return x.Select(c => new Tile(c)).ToArray();
+            })
+            .ToArray();
+
+        start = current;
+        return grid;
+    }
+
+    static Direction CalculateNextDirection(Direction currentDirection)
+    {
+        return currentDirection switch
+        {
+            Direction.North => Direction.East,
+            Direction.East => Direction.South,
+            Direction.South => Direction.West,
+            Direction.West => Direction.North,
+            _ => throw new InvalidOperationException("Only NESW directions are possible!")
+        };
+    }
+
+    static (int, int) CalculateNextMove((int, int) currentPos, Direction direction)
+    {
+        return direction switch
+        {
+            Direction.North => (currentPos.Item1 - 1, currentPos.Item2),
+            Direction.South => (currentPos.Item1 + 1, currentPos.Item2),
+            Direction.East => (currentPos.Item1, currentPos.Item2 + 1),
+            Direction.West => (currentPos.Item1, currentPos.Item2 - 1),
+            _ => throw new InvalidOperationException("Only NESW directions are possible!")
+        };
+    }
+
+    static bool IsValidTile(Tile[][] grid, (int, int) move)
+    {
+        return move.Item1 >= 0
+            && move.Item1 < grid.Length
+            && move.Item2 >= 0
+            && move.Item2 < grid[0].Length;
+    }
+
+    static bool IsLoop(Tile[][] grid, (int, int) currentPos, (int, int) potentialObstaclePos, Direction beamDirection)
+    {
+        //We set potential obstacle before checking for loop
+        grid[potentialObstaclePos.Item1][potentialObstaclePos.Item2].Char = '#';
+
+        var traversedObstacles = new List<(int, int, Direction)>();
+
+        while (IsValidTile(grid, currentPos))
+        {
+            var inFrontOfBeam = CalculateNextMove(currentPos, beamDirection);
+
+            if (!IsValidTile(grid, inFrontOfBeam))
+                return false;
+
+            while (grid[inFrontOfBeam.Item1][inFrontOfBeam.Item2].Char is '#')
+            {
+                if (traversedObstacles.Contains((inFrontOfBeam.Item1, inFrontOfBeam.Item2, beamDirection)))
+                    return true;
+
+                traversedObstacles.Add((inFrontOfBeam.Item1, inFrontOfBeam.Item2, beamDirection));
+
+                beamDirection = CalculateNextDirection(beamDirection);
+
+                inFrontOfBeam = CalculateNextMove(currentPos, beamDirection);
+            }
+
+            currentPos = CalculateNextMove(currentPos, beamDirection);
+        }
+
+        return false;
+    }
+
+    struct Tile(char c)
+    {
+        public char Char = c;
+        public bool Traversed = false;
+    }
 }
 
---
+++ 2024/AOC2024/Utility/Extensions/ArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Utility.Extensions;
+public static class ArrayExtensions
+{
+    public static T[][] DeepCopy<T>(T[][] source)
+    {
+        T[][] copy = new T[source.Length][];
+
+        for (int i = 0; i < source.Length; i++)
+        {
+            copy[i] = new T[source[i].Length];
+            System.Array.Copy(source[i], copy[i], source[i].Length);
+        }
+
+        return copy;
+    }
+}
