1fcfb449aa43513cc19adaaeffec7e33bb1e4596
Nikola Stanojkovic <nikola.stanojkovic@naissus.tech>
2025-01-06 14:54:30 +0100

    Day12 WIP

---
+++ 2024/AOC2024/Day12/Solution.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace Day12;
+public class Solution
+{
+    [Test]
+    public void Part1_Example()
+    {
+        var result = SolvePart1("Inputs/example.txt");
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Part1_Input()
+    {
+        var result = SolvePart1("Inputs/input.txt");
+        Console.WriteLine(result);
+    }
+
+    [Test]
+    public void Part2_Example()
+    {
+        var result = SolvePart2("Inputs/example.txt");
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Part2_Input()
+    {
+        var result = SolvePart2("Inputs/input.txt");
+        Console.WriteLine(result);
+    }
+
+    int SolvePart1(string inputPath)
+    {
+        return 0;
+    }
+
+    int SolvePart2(string inputPath)
+    {
+        return 0;
+    }
+}
