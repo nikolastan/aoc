9550b241d94b67820abc14b32b09f6090c21ae06
nikola.stanojkovic <nikolastanojkovic3@gmail.com>
2025-01-10 22:41:30 +0100

    Update .gitignore, remove ignored files, add proj for Day13

---
+++ 2024/AOC2024/Day13/Solution.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace Day13;
+public partial class Solution
+{
+	[Test]
+	public void Part1_Example()
+	{
+		var result = SolvePart1("Inputs/example.txt");
+		Assert.That(result, Is.EqualTo(0));
+	}
+
+	[Test]
+	public void Part1_Input()
+	{
+		var result = SolvePart1("Inputs/input.txt");
+		Console.WriteLine(result);
+	}
+
+	[Test]
+	public void Part2_Example()
+	{
+		var result = SolvePart2("Inputs/example.txt");
+		Assert.That(result, Is.EqualTo(0));
+	}
+
+	[Test]
+	public void Part2_Input()
+	{
+		var result = SolvePart2("Inputs/input.txt");
+		Console.WriteLine(result);
+	}
+
+	int SolvePart1(string inputPath)
+	{
+		return 0;
+	}
+
+	int SolvePart2(string inputPath)
+	{
+		return 0;
+	}
+}
+
