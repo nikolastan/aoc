3887f68d9dedd99052c2d5dfe3d38d91222dbb36
nikola.stanojkovic <nikolastanojkovic3@gmail.com>
2025-01-22 22:17:59 +0100

    Solve Day17 pt1

--- 2024/AOC2024/Day17/Solution.cs
+++ 2024/AOC2024/Day17/Solution.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.Win32;
+using NUnit.Framework;
 
 namespace Day17;
 public class Solution
@@ -7,7 +9,7 @@ public class Solution
     public void Part1_Example()
     {
         var result = SolvePart1("Inputs/example.txt");
-        Assert.That(result, Is.EqualTo(0));
+        Assert.That(result, Is.EqualTo("4,6,3,5,6,3,5,2,1,0"));
     }
 
     [Test]
@@ -31,9 +33,11 @@ public class Solution
         Console.WriteLine(result);
     }
 
-    int SolvePart1(string inputPath)
+    static string SolvePart1(string inputPath, int? regAOverride = null)
     {
-        return 0;
+        (var registers, var program) = ReadInput(inputPath);
+
+        return ExecuteProgram(program, registers);
     }
 
     int SolvePart2(string inputPath)
@@ -41,18 +45,70 @@ public class Solution
         return 0;
     }
 
-    static int? Execute(int opcode, int operand, Registers registers)
+    static (Registers Registers, List<int> Program) ReadInput(string inputPath)
+    {
+        var rawInput = File.ReadAllLines(inputPath)
+            .Where(x => x is not "")
+            .Select(x => x.Split(':'))
+            .Select(x => x[1].Trim())
+            .ToList();
+
+
+        var registers = new Registers(int.Parse(rawInput[0]), int.Parse(rawInput[1]), int.Parse(rawInput[2]));
+        var programs = rawInput[3].Split(',')
+            .Select(int.Parse)
+            .ToList();
+
+        return (registers, programs);
+    }
+	static string ExecuteProgram(List<int> program, Registers registers, int? regAOverride = null)
+    {
+		if (regAOverride is not null)
+			registers.A = regAOverride.Value;
+
+		var result = "";
+		var instructionPointer = 0;
+		while (instructionPointer < program.Count)
+		{
+			var currentOperation = program[instructionPointer];
+
+			var currentOperand = program[instructionPointer + 1];
+
+			switch (currentOperation)
+			{
+				case 5:
+					result += $"{ExecuteOp(currentOperation, currentOperand, registers)},";
+					break;
+				case 3:
+					if (registers.A is not 0)
+					{
+						instructionPointer = currentOperand;
+						continue;
+					}
+					break;
+				default:
+					ExecuteOp(currentOperation, currentOperand, registers);
+					break;
+			}
+
+			instructionPointer += 2;
+		}
+
+		return result[..^1];
+	}
+
+    static int? ExecuteOp(int opcode, int operand, Registers registers)
     {
         return opcode switch
         {
-            0 => registers.A /= (int)Math.Pow(ParseOperand(OperandType.Combo, operand, registers), 2), //adv
-            1 => registers.B ^ ParseOperand(OperandType.Literal, operand, registers), //bxl
+            0 => registers.A /= (int)Math.Pow(2, ParseOperand(OperandType.Combo, operand, registers)), //adv
+            1 => registers.B ^= ParseOperand(OperandType.Literal, operand, registers), //bxl
             2 => registers.B = ParseOperand(OperandType.Combo, operand, registers) % 8, //bst
             3 => registers.A is not 0 ? 1 : 0, //jnz
             4 => registers.B ^= registers.C, //bxc
             5 => ParseOperand(OperandType.Combo, operand, registers) % 8,
-            6 => registers.B = registers.A / (int)Math.Pow(ParseOperand(OperandType.Combo, operand, registers), 2), //bdv
-            7 => registers.C = registers.A / (int)Math.Pow(ParseOperand(OperandType.Combo, operand, registers), 2), //cdv
+            6 => registers.B = registers.A / (int)Math.Pow(2, ParseOperand(OperandType.Combo, operand, registers)), //bdv
+            7 => registers.C = registers.A / (int)Math.Pow(2, ParseOperand(OperandType.Combo, operand, registers)), //cdv
             _ => throw new NotImplementedException()
         };
     }
