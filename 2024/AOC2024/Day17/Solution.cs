eb2171512e366eff8dedbc98e8fae6fc47926744
Nikola Stanojkovic <nikolastanojkovic3@gmail.com>
2025-01-30 16:34:08 +0100

    Day17 pt2 WIP

--- 2024/AOC2024/Day17/Solution.cs
+++ 2024/AOC2024/Day17/Solution.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
-using Microsoft.Win32;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System.Linq.Expressions;
 
 namespace Day17;
 public class Solution
@@ -17,16 +16,16 @@ public class Solution
     {
         var result = SolvePart1("Inputs/input.txt");
         Console.WriteLine(result);
-	}
+    }
 
-	[Test]
-	public void Part1_InputTest1()
-	{
-		var result = SolvePart1("Inputs/input.txt", 2072305);
-		Console.WriteLine(result);
-	}
+    [Test]
+    public void Part1_InputTest1()
+    {
+        var result = SolvePart1("Inputs/input.txt", 2072305);
+        Console.WriteLine(result);
+    }
 
-	[Test]
+    [Test]
     public void Part2_Example2()
     {
         var result = SolvePart2("Inputs/example2.txt");
@@ -47,41 +46,21 @@ public class Solution
         Console.WriteLine(result);
     }
 
-    static string SolvePart1(string inputPath, int? regAOverride = null)
+    static string SolvePart1(string inputPath, long? regAOverride = null)
     {
         (var registers, var program) = ReadInput(inputPath);
 
         return ExecuteProgram(program, registers, regAOverride);
     }
 
-    int SolvePart2(string inputPath)
+    long SolvePart2(string inputPath)
     {
-		(var registers, var program) = ReadInput(inputPath);
+        (var registers, var program) = ReadInput(inputPath);
 
-        var increments = new List<int>();
+        var registerExp = new Dictionary<char, Expression<Func<int, long>>>();
 
-        var instructionPointer = 0;
-        while(instructionPointer < program.Count)
-        {
-            if (program[instructionPointer] is 0)
-                increments.Add(program[instructionPointer + 1]);
-            instructionPointer += 2;
-        }
-        increments.Reverse();
-
-        var numOfIncrementsAdded = program.Count;
-
-		var currentRegA = increments
-				.Select(inc => (int)Math.Pow(2, inc * numOfIncrementsAdded))
-				.Aggregate((x, y) => x * y);
-
-		if (currentRegA < 0)
-			currentRegA = int.MaxValue;
-        //TODO Debug part of determining starting value of reg A
-		ExecuteProgramUntilMatch(program, registers, ref currentRegA);
-
-		return currentRegA - 7;
-	}
+        return 0;
+    }
 
     static (Registers Registers, List<int> Program) ReadInput(string inputPath)
     {
@@ -91,8 +70,8 @@ public class Solution
             .Select(x => x[1].Trim())
             .ToList();
 
+        var registers = new Registers(long.Parse(rawInput[0]), long.Parse(rawInput[1]), long.Parse(rawInput[2]));
 
-        var registers = new Registers(int.Parse(rawInput[0]), int.Parse(rawInput[1]), int.Parse(rawInput[2]));
         var programs = rawInput[3].Split(',')
             .Select(int.Parse)
             .ToList();
@@ -100,107 +79,59 @@ public class Solution
         return (registers, programs);
     }
 
-	static string ExecuteProgram(List<int> program, Registers registers, int? regAOverride = null)
+    static string ExecuteProgram(List<int> program, Registers registers, long? regAOverride = null)
     {
-		if (regAOverride is not null)
-			registers.A = regAOverride.Value;
+        if (regAOverride is not null)
+            registers.A = regAOverride.Value;
 
-		var result = "";
-		var instructionPointer = 0;
-		while (instructionPointer < program.Count)
-		{
-			var currentOperation = program[instructionPointer];
-
-			var currentOperand = program[instructionPointer + 1];
-
-			switch (currentOperation)
-			{
-				case 5:
-					result += $"{ExecuteOp(currentOperation, currentOperand, registers)},";
-					break;
-				case 3:
-					if (registers.A is not 0)
-					{
-						instructionPointer = currentOperand;
-						continue;
-					}
-					break;
-				default:
-					ExecuteOp(currentOperation, currentOperand, registers);
-					break;
-			}
-
-			instructionPointer += 2;
-		}
-
-		return result[..^1];
-	}
-
-    static void ExecuteProgramUntilMatch(List<int> program, Registers registers, ref int startingRegAValue)
-    {
-		var result = new List<int>();
-		while (result.Count != program.Count || startingRegAValue > 0)
+        var result = "";
+        var instructionPointer = 0;
+        while (instructionPointer < program.Count)
         {
-			registers.A = startingRegAValue;
-            registers.B = 0;
-            registers.C = 0;
+            var currentOperation = program[instructionPointer];
 
-			var instructionPointer = 0;
-			while (instructionPointer < program.Count)
-			{
-				var currentOperation = program[instructionPointer];
+            var currentOperand = program[instructionPointer + 1];
 
-				var currentOperand = program[instructionPointer + 1];
+            switch (currentOperation)
+            {
+                case 5:
+                    result += $"{ExecuteOp(currentOperation, currentOperand, registers)},";
+                    break;
+                case 3:
+                    if (registers.A is not 0)
+                    {
+                        instructionPointer = currentOperand;
+                        continue;
+                    }
+                    break;
+                default:
+                    ExecuteOp(currentOperation, currentOperand, registers);
+                    break;
+            }
 
-				switch (currentOperation)
-				{
-					case 5:
-						var value = ExecuteOp(currentOperation, currentOperand, registers)!.Value;
-						if (program[result.Count] != value)
-						{
-							instructionPointer = program.Count;
-                            result = [];
-                            startingRegAValue--;
-							continue;
-						}
-						else
-							result.Add(value);
+            instructionPointer += 2;
+        }
 
-						break;
-					case 3:
-						if (registers.A is not 0)
-						{
-							instructionPointer = currentOperand;
-							continue;
-						}
-						break;
-					default:
-						ExecuteOp(currentOperation, currentOperand, registers);
-						break;
-				}
+        return result[..^1];
+    }
 
-				instructionPointer += 2;
-			}
-		}
-	}
-
-    static int? ExecuteOp(int opcode, int operand, Registers registers)
+    static long? ExecuteOp(long opcode, int operand, Registers registers)
     {
         return opcode switch
         {
-            0 => registers.A /= (int)Math.Pow(2, ParseOperand(OperandType.Combo, operand, registers)), //adv
+            0 => registers.A /= (long)Math.Pow(2, ParseOperand(OperandType.Combo, operand, registers)), //adv
             1 => registers.B ^= ParseOperand(OperandType.Literal, operand, registers), //bxl
             2 => registers.B = ParseOperand(OperandType.Combo, operand, registers) % 8, //bst
             3 => 0, //jnz
             4 => registers.B ^= registers.C, //bxc
             5 => ParseOperand(OperandType.Combo, operand, registers) % 8, //out
-            6 => registers.B = registers.A / (int)Math.Pow(2, ParseOperand(OperandType.Combo, operand, registers)), //bdv
-            7 => registers.C = registers.A / (int)Math.Pow(2, ParseOperand(OperandType.Combo, operand, registers)), //cdv
+            6 => registers.B = registers.A / (long)Math.Pow(2, ParseOperand(OperandType.Combo, operand, registers)), //bdv
+            7 => registers.C = registers.A / (long)Math.Pow(2, ParseOperand(OperandType.Combo, operand, registers)), //cdv
             _ => throw new NotImplementedException()
         };
     }
 
-    static int ParseOperand(OperandType operandType, int operand, Registers registers)
+    static long ParseOperand(OperandType operandType, int operand, Registers registers)
     {
         return operandType switch
         {
@@ -210,7 +141,7 @@ public class Solution
         };
     }
 
-    static int ParseComboOperand(int operand, Registers registers)
+    static long ParseComboOperand(int operand, Registers registers)
     {
         return operand switch
         {
@@ -223,11 +154,13 @@ public class Solution
         };
     }
 
-    class Registers(int regA, int regB, int regC)
+
+
+    class Registers(long regA, long regB, long regC)
     {
-        public int A = regA;
-        public int B = regB;
-        public int C = regC;
+        public long A = regA;
+        public long B = regB;
+        public long C = regC;
     }
 
     enum OperandType
