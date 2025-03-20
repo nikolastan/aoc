f9d8ebc562642e84e9dd4f3aca1c8b699ec03fac
Nikola Stanojkovic <nikolastanojkovic3@gmail.com>
2025-03-20 15:46:58 +0100

    Day18 pt2 WIP

--- 2024/AOC2024/Day18/Solution.cs
+++ 2024/AOC2024/Day18/Solution.cs
@@ -21,17 +21,18 @@ public class Solution
     [Test]
     public void Part2_Example()
     {
-        var result = SolvePart2("Inputs/example.txt");
-        Assert.That(result, Is.EqualTo(0));
+        var result = SolvePart2("Inputs/example.txt", 7, 12);
+        Assert.That(result, Is.EqualTo("6,1"));
     }
 
     [Test]
     public void Part2_Input()
     {
-        var result = SolvePart2("Inputs/input.txt");
+        var result = SolvePart2("Inputs/input.txt", 71, 1024);
         Console.WriteLine(result);
     }
-    int SolvePart1(string inputPath, int mapDim, int numOfBytesFallen)
+
+    static int SolvePart1(string inputPath, int mapDim, int numOfBytesFallen)
     {
         var map = ReadMap(inputPath, mapDim, numOfBytesFallen);
         Dictionary<(int, int), int> memo = [];
@@ -40,26 +41,66 @@ public class Solution
         return memo[(mapDim - 1, mapDim - 1)];
     }
 
-    int SolvePart2(string inputPath)
+    static string SolvePart2(string inputPath, int mapDim, int numOfBytesFallen)
     {
-        return 0;
+        var map = ReadMap(inputPath, mapDim, numOfBytesFallen);
+
+        var fallingBytes = ReadFallingBytes(inputPath, numOfBytesFallen);
+
+        List<(int X, int Y)> blockingBytes = [];
+        FindBlockingBytes(map, (0, 0), (mapDim - 1, mapDim - 1), [], [], blockingBytes);
+
+        (int X, int Y)? lastByte = null;
+        foreach (var @byte in fallingBytes)
+        {
+            if (blockingBytes.Contains(@byte))
+                lastByte = @byte;
+            else
+                break;
+        }
+
+        if (lastByte is null)
+            throw new InvalidOperationException("Byte that prevents exit was not found!");
+
+        return $"{lastByte.Value.X},{lastByte.Value.Y}";
     }
 
     static char[][] ReadMap(string inputPath, int mapDim, int numOfBytesFallen)
     {
-        var byteCoordinates = File.ReadAllLines(inputPath)
-            .Select(x => x.Split(','))
-            .Select(x => (int.Parse(x[0]), int.Parse(x[1])))
+        var parsedBytes = ParseByteCoordinates(File.ReadAllLines(inputPath));
+
+        var fallenBytes = parsedBytes
             .Take(numOfBytesFallen)
+            .ToList();
+
+        var upBytes = parsedBytes
+            .Skip(numOfBytesFallen)
             .ToList();
 
         return Enumerable.Range(0, mapDim)
             .Select(x => new char[mapDim])
-            .Select((row, i) => row.Select((col, j) => byteCoordinates.Contains((i, j))
-                    ? '#'
-                    : '.')
-                .ToArray())
+            .Select((row, i) => row.Select((_, j) =>
+            {
+                if (fallenBytes.Contains((i, j)))
+                    return '#';
+                if (upBytes.Contains((i, j)))
+                    return '?';
+
+                return '.';
+            }).ToArray())
             .ToArray();
+    }
+
+    static IEnumerable<(int X, int Y)> ReadFallingBytes(string inputPath, int skipBytes)
+    {
+        return ParseByteCoordinates(File.ReadAllLines(inputPath))
+            .Skip(skipBytes);
+    }
+
+    static IEnumerable<(int, int)> ParseByteCoordinates(string[] rawCoordinates)
+    {
+        return rawCoordinates.Select(x => x.Split(','))
+            .Select(x => (int.Parse(x[0]), int.Parse(x[1])));
     }
 
     static void FindMinSteps(
@@ -96,6 +137,59 @@ public class Solution
         if (ArrayExtensions.IsValidTile(map, nextPos))
             FindMinSteps(map, nextPos, end, currentSteps + 1, memo);
     }
+
+    static void FindBlockingBytes(
+        char[][] map,
+        (int X, int Y) currentPos,
+        (int X, int Y) end,
+        List<(int, int)> currentRoute,
+        List<(int, int)> currentBlockingBytes,
+        List<(int, int)> totalBlockingBytes)
+    {
+        if (map[currentPos.X][currentPos.Y] is '#')
+            return;
+
+        if (currentRoute.Contains(currentPos))
+            return;
+        else
+            currentRoute.Add(currentPos);
+
+        if (map[currentPos.X][currentPos.Y] is '?' && !currentBlockingBytes.Contains(currentPos))
+        {
+            currentBlockingBytes.Add(currentPos);
+        }
+
+        if (currentPos == end)
+        {
+            totalBlockingBytes.AddRange(currentBlockingBytes);
+            return;
+        }
+
+        var nextPos = (currentPos.X + 1, currentPos.Y);
+        if (ArrayExtensions.IsValidTile(map, nextPos))
+            FindBlockingBytes(map, nextPos, end,
+                new List<(int, int)>(currentRoute),
+                new List<(int, int)>(currentBlockingBytes), totalBlockingBytes);
+
+        nextPos = (currentPos.X, currentPos.Y + 1);
+        if (ArrayExtensions.IsValidTile(map, nextPos))
+            FindBlockingBytes(map, nextPos, end,
+                new List<(int, int)>(currentRoute),
+                new List<(int, int)>(currentBlockingBytes),
+                totalBlockingBytes);
+
+        nextPos = (currentPos.X - 1, currentPos.Y);
+        if (ArrayExtensions.IsValidTile(map, nextPos))
+            FindBlockingBytes(map, nextPos, end,
+                new List<(int, int)>(currentRoute),
+                new List<(int, int)>(currentBlockingBytes),
+                totalBlockingBytes);
+
+        nextPos = (currentPos.X, currentPos.Y - 1);
+        if (ArrayExtensions.IsValidTile(map, nextPos))
+            FindBlockingBytes(map, nextPos, end,
+                new List<(int, int)>(currentRoute),
+                new List<(int, int)>(currentBlockingBytes),
+                totalBlockingBytes);
+    }
 }
-
-
