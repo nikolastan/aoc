4aa596abe04f5d593806e886e26d90f96a17d8bd
Nikola Stanojkovic <nikolastanojkovic3@gmail.com>
2025-04-01 16:27:16 +0200

    Day21 pt1 solve, pt2 WIP

--- 2024/AOC2024/Day21/Solution.cs
+++ 2024/AOC2024/Day21/Solution.cs
@@ -4,120 +4,140 @@ using Utility.Extensions;
 namespace Day21;
 public class Solution
 {
-    readonly char[,] NumericKeyboard = new char[4, 3]
+    readonly (char[,] Map, (int X, int Y) Start) NumericKeyboard = (new char[4, 3]
     {
         {'7', '8', '9' },
         {'4', '5', '6' },
         {'1', '2', '3' },
         {'X', '0', 'A' }
-    };
+    }, (3, 2));
 
-    readonly char[,] DirectionalKeyboard = new char[2, 3]
+    readonly (char[,] Map, (int X, int Y) Start) DirectionalKeyboard = (new char[2, 3]
     {
         {'X', '^', 'A' },
         {'<', 'v', '>' }
-    };
+    }, (0, 2));
 
     [Test]
     public void Part1_Example()
     {
-        var result = SolvePart1("Inputs/example.txt");
-        Assert.That(result, Is.EqualTo(126384));
+        var result = Solve("Inputs/example.txt", 3);
+        Assert.That((int)result, Is.EqualTo(126384));
     }
 
     [Test]
     public void Part1_Input()
     {
-        var result = SolvePart1("Inputs/input.txt");
+        var result = Solve("Inputs/input.txt", 3);
         Console.WriteLine(result);
     }
 
     [Test]
     public void Part2_Example()
     {
-        var result = SolvePart2("Inputs/example.txt");
+        var result = Solve("Inputs/example.txt", 10);
         Assert.That(result, Is.EqualTo(0));
     }
 
     [Test]
     public void Part2_Input()
     {
-        var result = SolvePart2("Inputs/input.txt");
+        var result = Solve("Inputs/input.txt", 25);
         Console.WriteLine(result);
     }
 
-    int SolvePart1(string inputPath)
+    Int128 Solve(string inputPath, int iterations)
     {
         var codes = File.ReadLines(inputPath);
-        var result = 0;
+        Int128 result = 0;
 
         foreach (var code in codes)
         {
-            var first = Translate(NumericKeyboard, (3, 2), code);
-            var second = Translate(DirectionalKeyboard, (0, 2), first);
-            var third = Translate(DirectionalKeyboard, (0, 2), second);
+            var translation = Translate(code, 0, iterations, []);
 
             var numericPart = int.Parse(code[0..3]);
 
-            result += numericPart * third.Length;
+            result += numericPart * translation.Length;
         }
 
         return result;
     }
 
-    int SolvePart2(string inputPath)
+    string Translate(string code, int iteration, int totalTranslations, Dictionary<(string, int), string> memo)
     {
-        return 0;
-    }
+        if (totalTranslations == iteration)
+            return code;
 
-    string Translate(char[,] keyboard, (int X, int Y) start, string numericCode)
-    {
+        if (memo.TryGetValue((code, iteration), out string? currentMin))
+            return currentMin;
+
+        var (Map, Start) = iteration is 0
+            ? NumericKeyboard
+            : DirectionalKeyboard;
+
+        var currentTile = Start;
         var result = "";
-        (int X, int Y) currentTile = start;
 
-        foreach (var codeChar in numericCode)
+        foreach (var codeChar in code)
         {
-            var goalCoordinates = GetTileCoordinates(keyboard, codeChar);
-            var memo = new Dictionary<(int X, int Y), List<(int X, int Y)>>();
-            FindShortestRoute(keyboard, currentTile, goalCoordinates, [], memo);
+            var endTile = GetTileCoordinates(Map, codeChar);
 
-            var directionVectors = memo[goalCoordinates][..^1]
-                .Zip(memo[goalCoordinates][1..])
-                .Select(x => (x.Second.X - x.First.X, x.Second.Y - x.First.Y))
-                .Cast<(int X, int Y)>()
-                .ToList();
+            var translations = EncodeDirections(Map, currentTile, endTile);
+            var shortestTranslation = translations
+                .Select(x => Translate(x, iteration + 1, totalTranslations, memo))
+                .OrderBy(x => x.Length)
+                .First();
 
-            var translatedCode = TranslateDirectionsToCode(directionVectors);
-
-            result += string.Concat<char>([.. translatedCode, 'A']);
-            currentTile = goalCoordinates;
+            result += shortestTranslation;
+            currentTile = endTile;
         }
+
+        memo[(code, iteration)] = result;
 
         return result;
     }
 
-    void FindShortestRoute(
+    List<string> EncodeDirections(char[,] keyboard, (int X, int Y) startTile, (int X, int Y) endTile)
+    {
+        var memo = new Dictionary<(int X, int Y), List<(int X, int Y)[]>>();
+        FindAllShortestRoutes(keyboard, startTile, endTile, [], memo);
+
+        var directionVectors = memo[endTile]
+            .Select(x => x[..^1]
+            .Zip(x[1..])
+            .Select(x => (x.Second.X - x.First.X, x.Second.Y - x.First.Y))
+            .Cast<(int X, int Y)>()
+            .ToList());
+
+        return directionVectors
+            .Select(x => new string(TranslateDirectionsToCode(x).ToArray()))
+            .ToList();
+    }
+
+    void FindAllShortestRoutes(
         char[,] map,
         (int X, int Y) currentTile,
         (int X, int Y) goal,
         List<(int X, int Y)> currentRoute,
-        Dictionary<(int X, int Y), List<(int X, int Y)>> memo)
+        Dictionary<(int X, int Y), List<(int X, int Y)[]>> memo)
     {
         if (currentRoute.Contains(currentTile)
             || !ArrayExtensions.IsValidTile(map, currentTile)
             || map[currentTile.X, currentTile.Y] == 'X')
             return;
 
-        if (!memo.TryGetValue(currentTile, out var currentMin) || currentMin.Count - 1 > currentRoute.Count)
-            memo[currentTile] = [.. currentRoute, currentTile];
+        if (!memo.TryGetValue(currentTile, out var currentMin) || currentMin.First().Length - 1 > currentRoute.Count)
+            memo[currentTile] = [[.. currentRoute, currentTile]];
+        else if (currentMin.Count > 0 && currentMin.First().Length - 1 == currentRoute.Count)
+            memo[currentTile].Add([.. currentRoute, currentTile]);
 
         if (currentTile == goal)
             return;
 
-        FindShortestRoute(map, (currentTile.X + 1, currentTile.Y), goal, [.. currentRoute, currentTile], memo);
-        FindShortestRoute(map, (currentTile.X, currentTile.Y + 1), goal, [.. currentRoute, currentTile], memo);
-        FindShortestRoute(map, (currentTile.X - 1, currentTile.Y), goal, [.. currentRoute, currentTile], memo);
-        FindShortestRoute(map, (currentTile.X, currentTile.Y - 1), goal, [.. currentRoute, currentTile], memo);
+        FindAllShortestRoutes(map, (currentTile.X + 1, currentTile.Y), goal, [.. currentRoute, currentTile], memo);
+        FindAllShortestRoutes(map, (currentTile.X, currentTile.Y + 1), goal, [.. currentRoute, currentTile], memo);
+        FindAllShortestRoutes(map, (currentTile.X - 1, currentTile.Y), goal, [.. currentRoute, currentTile], memo);
+        FindAllShortestRoutes(map, (currentTile.X, currentTile.Y - 1), goal, [.. currentRoute, currentTile], memo);
     }
 
     (int X, int Y) GetTileCoordinates(char[,] map, char tile)
@@ -145,5 +165,7 @@ public class Solution
                 _ => throw new InvalidOperationException($"Invalid vector {dirVec}.")
             };
         }
+
+        yield return 'A';
     }
 }
