2c6dc48b74cd5df30cc7150c1e7299a98569bccd
Nikola Stanojkovic <nikolastanojkovic3@gmail.com>
2025-05-12 13:38:11 +0200

    Day22 pt2 solve

--- 2024/AOC2024/Day22/Solution.cs
+++ 2024/AOC2024/Day22/Solution.cs
@@ -20,8 +20,8 @@ public class Solution
     [Test]
     public void Part2_Example()
     {
-        var result = SolvePart2("Inputs/example.txt");
-        Assert.That(result, Is.EqualTo(0));
+        var result = SolvePart2("Inputs/example2.txt");
+        Assert.That(result, Is.EqualTo(23));
     }
 
     [Test]
@@ -38,25 +38,58 @@ public class Solution
             .ToList();
 
         foreach (var _ in Enumerable.Range(0, 2000))
-        {
             initialNumbers = initialNumbers
-                .Select((x, i) => ((x * 64) ^ initialNumbers[i]) % 16777216)
-                .ToList();
-
-            initialNumbers = initialNumbers
-                .Select((x, i) => ((x / 32) ^ initialNumbers[i]) % 16777216)
-                .ToList();
-
-            initialNumbers = initialNumbers
-                .Select((x, i) => ((x * 2048) ^ initialNumbers[i]) % 16777216)
-                .ToList();
-        }
+                    .Select(GenerateNextSecret)
+                    .ToList();
 
         return initialNumbers.Sum();
     }
 
     int SolvePart2(string inputPath)
     {
-        return 0;
+        var currentSecrets = File.ReadAllLines(inputPath)
+            .Select(long.Parse)
+            .ToList();
+
+        var globalMemo = new Dictionary<string, int>();
+
+        foreach (var vendorIndex in Enumerable.Range(0, currentSecrets.Count))
+        {
+            var vendorMemo = new HashSet<string>();
+            var diffs = new List<char>();
+
+            foreach (var secretIndex in Enumerable.Range(0, 2000))
+            {
+                var nextSecret = GenerateNextSecret(currentSecrets[vendorIndex]);
+                int lastPrice = (int)nextSecret % 10;
+                diffs.Add((char)(lastPrice - (currentSecrets[vendorIndex] % 10)));
+                currentSecrets[vendorIndex] = nextSecret;
+
+                if (secretIndex >= 4) //We start from 5th price because monkey cannot sell before that
+                {
+                    var diffSequence = new string(diffs.TakeLast(4).ToArray());
+                    if (!vendorMemo.TryGetValue(diffSequence, out _))
+                    {
+                        vendorMemo.Add(diffSequence);
+
+                        if (globalMemo.TryGetValue(diffSequence, out _))
+                            globalMemo[diffSequence] += lastPrice;
+                        else
+                            globalMemo.Add(diffSequence, lastPrice);
+                    }
+                }
+            }
+        }
+
+        return globalMemo.Max(x => x.Value);
+    }
+
+    long GenerateNextSecret(long currentSecret)
+    {
+        currentSecret = ((currentSecret * 64) ^ currentSecret) % 16777216;
+        currentSecret = ((currentSecret / 32) ^ currentSecret) % 16777216;
+        currentSecret = ((currentSecret * 2048) ^ currentSecret) % 16777216;
+
+        return currentSecret;
     }
 }
